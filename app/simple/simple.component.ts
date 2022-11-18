75c37d2efea301862b357ada9fd0a0f6c2313204
Saronik5883 <82265518+Saronik5883@users.noreply.github.com>
2022-11-18 19:14:21 +0530

    3 files modified

--- app/simple/simple.component.ts
+++ app/simple/simple.component.ts
@@ -1,36 +1,38 @@
-import { Component,ViewEncapsulation } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'simple-int',
   templateUrl: './simple.component.html',
-  styleUrls: [ './simple.component.css' ],
-  encapsulation : ViewEncapsulation.None
+  styleUrls: ['./simple.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SimpleComponent {
-
-  principal:number;
+  principal: number;
   interestRate: number;
   period: number;
 
-  interestAmount:number;
-  totalAmount:number;
+  interestAmount: number;
+  compoundinterest: number;
+  totalAmount: number;
+  n: number;
 
-  updatePrincipal(event){
+  updatePrincipal(event) {
     this.principal = event.target.value;
   }
 
-  updateInterestRate(event){
+  updateInterestRate(event) {
     this.interestRate = event.target.value;
   }
 
-  updatePeriod(event){
+  updatePeriod(event) {
     this.period = event.target.value;
   }
 
-  calcuateInterest(){
-    this.interestAmount = ((this.principal * this.interestRate) / 100 ) * this.period;
-    this.totalAmount = this.principal * (1 + (this.interestRate/100 * this.period));
+  calcuateInterest() {
+    this.interestAmount =
+      ((this.principal * this.interestRate) / 100) * this.period;
+    this.totalAmount =
+      this.principal * Math.pow(1 + this.interestRate / 12, 12 * this.period);
+    this.compoundinterest = this.totalAmount - this.principal;
   }
-
 }
